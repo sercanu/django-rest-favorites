7707fcc88bd6ef3b99a78df56395d7abbcba0784
sercan <sercan.ulucan@elkotek.com.tr>
2013-09-26 18:34:26 +0300

    api

--- core/models.py
+++ core/models.py
@@ -6,8 +6,8 @@ from model_utils.models import TimeStampedModel
 class Favorite(TimeStampedModel):
 
     owner = models.ForeignKey('auth.User', related_name='favorites')
-    url = models.CharField(max_length=1000)
+    content = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000)
 
     def __unicode__(self):
-        return self.url
+        return self.content
--- core/serializers.py
+++ core/serializers.py
@@ -6,35 +6,18 @@ from rest_framework import serializers
 from core.models import Favorite
 
 
-class FavoriteSerializer(serializers.Serializer):
+class FavoriteSerializer(serializers.HyperlinkedModelSerializer):
 
-    pk = serializers.Field()  # Note: `Field` is an untyped read-only field.
-    url = serializers.CharField(required=False, max_length=1000)
-    description = serializers.CharField(required=False, max_length=1000)
     owner = serializers.Field(source='owner.username')
-    created = serializers.DateTimeField(required=False)
 
-    def restore_object(self, attrs, instance=None):
-        """
-        Create or update a new snippet instance, given a dictionary
-        of deserialized field values.
-
-        Note that if we don't define this method, then deserializing
-        data will simply return a dictionary of items.
-        """
-        if instance:
-            # Update existing instance
-            instance.title = attrs.get('url', instance.url)
-            instance.description = attrs.get('code', instance.description)
-            return instance
-
-        # Create new instance
-        return Favorite(**attrs)
+    class Meta:
+        model = Favorite
+        fields = ('url', 'content', 'description', 'owner', 'created')
 
 
-class UserSerializer(serializers.ModelSerializer):
-    favorites = FavoriteSerializer(many=True)
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    favorites = serializers.HyperlinkedRelatedField(many=True, view_name='favorite-detail')
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'favorites')
+        fields = ('url', 'username', 'email', 'favorites')
