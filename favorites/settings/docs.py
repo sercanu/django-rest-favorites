f026b22609b90e24f4acd73fb78e77b5ff9fbb97
sercan <sercan.ulucan@elkotek.com.tr>
2013-10-09 20:32:35 +0300

    docs

--- favorites/settings/docs.py
+++ favorites/settings/docs.py
@@ -7,4 +7,13 @@ THIRD_PARTY_APPS = (
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
-DATABASES = {}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
