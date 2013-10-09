88693a0c792c37425aba4d0c441e9d0a65a0d01f
sercan <sercan.ulucan@elkotek.com.tr>
2013-10-09 20:04:44 +0300

    docs

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
-python setup.py install
+# coding=utf-8
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def read(*fname):
+    with open(os.path.join(os.path.dirname(__file__), *fname)) as f:
+        return f.read()
+
+
+def get_version():
+    for line in read('django-rest-favorites', '__init__.py').splitlines():
+        m = re.match(r"__version__\s*=\s'(.*)'", line)
+        if m:
+            return m.groups()[0].strip()
+    raise Exception('Cannot find version')
+
+
+setup(
+    name='django-rest-favorites',
+    version=get_version(),
+    author=u'sercanu',
+    author_email='sercanulucan.dev@gmail.com',
+    keywords='django rest',
+    url='https://github.com/sercanu/django-rest-favorites',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Django rest farmework sample',
+    long_description=read('README.rst'),
+    zip_safe=True,
+)
