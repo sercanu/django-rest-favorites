698b90326fd58d8311060d537fad259d30423798
sercan <sercan.ulucan@elkotek.com.tr>
2013-10-09 20:06:49 +0300

    docs

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
 # coding=utf-8
 import os
-import re
 from setuptools import setup, find_packages
 
 
 def read(*fname):
     with open(os.path.join(os.path.dirname(__file__), *fname)) as f:
         return f.read()
-
-
-def get_version():
-    for line in read('django-rest-favorites', '__init__.py').splitlines():
-        m = re.match(r"__version__\s*=\s'(.*)'", line)
-        if m:
-            return m.groups()[0].strip()
-    raise Exception('Cannot find version')
-
 
 setup(
     name='django-rest-favorites',
